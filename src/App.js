4bd54d8b7a35f57741cd045767a7e6a398bd2911
Jennifer Li <jeli@canadagoose.com>
2025-10-29 17:11:25 -0400

    update directory and category-item components

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
+import Directory from './components/directory/directory.component';
+
+import categories from './data/categories.json';
+
 const App = () => {
-
-  const categories = [
-    {
-      id: 1,
-      title: 'Hats'
-    },
-    {
-      id: 2,
-      title: 'Jackets'
-    },
-    {
-      id: 3,
-      title: 'Sneakers'
-    },
-    {
-      id: 4,
-      title: 'Womens'
-    },
-    {
-      id: 5,
-      title: 'Mens'
-    },
-  ];
-
   return (
-    <div className='categories-container'>
-      {categories.map(({title}) => (
-        <div className='category-container'>
-          <div className='background-image' />
-          <div className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
+    <Directory categories={categories} />
   );
 };
 
