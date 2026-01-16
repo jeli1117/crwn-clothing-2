2f0716a2ff1adae851ca5f33fbf0e6db0b7c08da
Jennifer Li <jeli@canadagoose.com>
2026-01-16 15:07:17 -0500

    create context for cart dropdown toggle

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,7 +6,7 @@ const CartDropdown = () => {
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'></div>
-            <Button>Go to checkout</Button>
+            <Button>go to checkout</Button>
         </div>
     )
 };
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
 import { ProductsProvider } from './contexts/products.context';
-import reportWebVitals from './reportWebVitals';
+import { CartProvider } from './contexts/cart.context';
 
 import './index.scss';
 
@@ -15,14 +15,11 @@ root.render(
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
